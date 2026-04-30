e6d0e42e03cf6c9f731332e9f7f1b53a20c67926
Samuel <samuelmorais552@gmail.com>
2026-04-30 00:17:43 -0300

    refactor:better typing and code strcuture

--- src/PO/ula.cpp
+++ src/PO/ula.cpp
@@ -9,48 +9,50 @@ SC_MODULE(ULA) {
     
 // Entrada
 sc_in<sc_uint<4>> op; /// Código da operação
-sc_in<sc_int<32>> operand_a; /// Primeiro operando
-sc_in<sc_int<32>> operand_b; /// Segundo operando
+sc_in<sc_uint<32>> operand_a; /// Primeiro operando
+sc_in<sc_uint<32>> operand_b; /// Segundo operando
 
 // Saidas
-sc_out<sc_int<32>> result; /// Resultado da operação aritmetica
+sc_out<sc_uint<32>> result; /// Resultado da operação aritmetica
 sc_out<bool> jump_cond; /// Flag que indica se o resultado é zero
 
 void process_ula() {
+    sc_int<32> op_a = sc_int<32>(operand_a.read());
+    sc_int<32> op_b = sc_int<32>(operand_b.read());
     sc_int<32> res;
     bool j_cond = false;
     switch (op.read()) {
         case 0b0000: // AND
-            res = operand_a.read() & operand_b.read();
+            res = op_a & op_b;
             break;
         case 0b0001: // ADD
-            res = operand_a.read() + operand_b.read();
+            res = op_a + op_b;
             break;
         case 0b0010: // SUB
-            res = operand_a.read() - operand_b.read();
+            res = op_a - op_b;
             break;
         case 0b0011: // OR
-            res = operand_a.read() | operand_b.read();
+            res = op_a | op_b;
             break;
         case 0b0100: // XOR
-            res = operand_a.read() ^ operand_b.read();
+            res = op_a ^ op_b;
             break;
         case 0b0101: // NOT
-            res = ~operand_a.read();
+            res = ~op_a;
             break;
         case 0b0110: // CMP
-            res = operand_a.read() == operand_b.read();
+            res = op_a == op_b;
             break;
         case 0b0111: // JZ
-            j_cond = operand_a.read() == 0;
+            j_cond = op_a == 0;
             break;
         case 0b1000: // JN
-            j_cond = operand_a.read() < 0;
+            j_cond = op_a < 0;
             break;
         default:
             break;
     }
-    result.write(res);
+    result.write(sc_uint<32>(res));
     jump_cond.write(j_cond);
 }
 
@@ -65,9 +67,9 @@ SC_CTOR(ULA) {
 // Simulacao
 int sc_main(int argc, char* argv[]) {
     sc_signal<sc_uint<4>> op;
-    sc_signal<sc_int<32>> operand_a;
-    sc_signal<sc_int<32>> operand_b;
-    sc_signal<sc_int<32>> result;
+    sc_signal<sc_uint<32>> operand_a;
+    sc_signal<sc_uint<32>> operand_b;
+    sc_signal<sc_uint<32>> result;
     sc_signal<bool> jump_cond;
 
     ULA ula("ula");
