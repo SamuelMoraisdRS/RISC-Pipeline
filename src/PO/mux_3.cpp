33a6dc1ac5d626ac722cf4ab8f4305f7e58e851c
Samuel <samuelmorais552@gmail.com>
2026-04-30 00:16:28 -0300

    refactor:Mux generic  word size

--- src/PO/mux_3.cpp
+++ src/PO/mux_3.cpp
@@ -5,16 +5,17 @@
 
 #include <systemc.h>
 
-SC_MODULE(MUX_3) {
+template <int WIDTH=32>
+struct MUX_3 : public sc_core::sc_module {
     
     // Entradas
-    sc_in<sc_uint<32>> in0; /// Primeira entrada
-    sc_in<sc_uint<32>> in1; /// Segunda entrada
-    sc_in<sc_uint<32>> in2; /// Terceira entrada
+    sc_in<sc_uint<WIDTH>> in0; /// Primeira entrada
+    sc_in<sc_uint<WIDTH>> in1; /// Segunda entrada
+    sc_in<sc_uint<WIDTH>> in2; /// Terceira entrada
     sc_in<sc_uint<2>> sel; /// Seletor de entrada
 
     // Saidas
-    sc_out<sc_uint<32>> out; /// Saída selecionada
+    sc_out<sc_uint<WIDTH>> out; /// Saída selecionada
 
     void process_mux() {
         switch (sel.read()) {
@@ -33,7 +34,8 @@ SC_MODULE(MUX_3) {
         }
     }
 
-    SC_CTOR(MUX_3) {
+    SC_HAS_PROCESS(MUX_3);
+    MUX_3(sc_core::sc_module_name name) : sc_core::sc_module(name) {
         SC_METHOD(process_mux);
         sensitive << in0 << in1 << in2 << sel;
     }
